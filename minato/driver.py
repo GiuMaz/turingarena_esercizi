545a5cb1878120c02c8c6a725784f218e128c386
Giulio Mazzi <mazzi.giulio2@gmail.com>
2017-10-12 21:50:38 +0200

    improved minato

--- minato/driver.py
+++ minato/driver.py
@@ -3,7 +3,7 @@ import sys
 from interfaces.minato import minato
 
 from turingarena.runtime.sandbox import sandbox
-from turingarena.runtime.data import rebased
+
 from random import seed, randint
 
 seed(1001001)
@@ -22,43 +22,111 @@ class minato_utils:
         self.mine_field = []
 
         for i in range(0,self.N): self.mine_field.append([1] * self.M)
+
+        for i in range(0,self.N):
+            for j in range(0, self.M):
+                if randint(0,99) > 85: self.mine_field[i][j] = 0
+
         n,m = 0,0
 
-        print(self.N,self.M)
         while n < self.N and m < self.M:
             self.mine_field[n][m] = 0
             if randint(0,1) == 0: n+=1
             else: m+=1
-            print(*self.mine_field,n,m,'\n',sep='\n')
 
         for i in range(n,self.N): self.mine_field[i][self.M-1] = 0
         for j in range(m,self.M): self.mine_field[self.N-1][j] = 0
 
-    def is_safe(self, row, column):
-        if column < 1 or column > self.M:
-            raise ValueError("column out of bound")
-        if row < 1 or row > self.N:
-            raise ValueError("row out of bound")
-
-        return self.mine_field[row-1][column-1]
-
 
 def evaluate_solution(N,M):
-    with sandbox.create_process("solution") as s, minato(s) as driver:
-
-        driver.N = N
-        driver.M = M
+    with sandbox.create_process("solution") as s, minato(s) as driver_solution,\
+            sandbox.create_process("reference") as r, minato(r) as driver_reference:
 
         mu = minato_utils(N,M)
-        driver.mine_field = mu.mine_field
-        S = driver.find_number_of_paths()
 
-        return S
+        driver_solution.N = N
+        driver_solution.M = M
+        driver_solution.mine_field = mu.mine_field
+        user_ans = driver_solution.find_number_of_paths()
 
-evaluate_solution(5,5)
+        driver_reference.N = N
+        driver_reference.M = M
+        driver_reference.mine_field = mu.mine_field
+        reference_ans = driver_reference.find_number_of_paths()
 
-#for i in range(0,20):
-#    with open("output/output{number}.txt".format(number=i),"r") as solution_file:
-#        ans = evaluate_solution("input/input{number}.txt".format(number=i))
-#        sol = int(solution_file.read())
-#        print(i, ans == sol )
+        return user_ans == reference_ans
+
+def evaluate_input_solution(input_file):
+    with sandbox.create_process("solution") as s, minato(s) as driver_solution,\
+            sandbox.create_process("reference") as r, minato(r) as driver_reference, \
+            open(input_file,"r") as f:
+
+        (N,M) = [int(x) for x in next(f).split()]
+        mine_map = []
+        for line in f:
+            mine_line = []
+            for c in line:
+                if c is '*': mine_line.append(0) 
+                if c is '+': mine_line.append(1) 
+            mine_map.append(mine_line)
+
+        mu = minato_utils(N,M,mine_map)
+
+        driver_solution.N = N
+        driver_solution.M = M
+        driver_solution.mine_field = mu.mine_field
+        user_ans = driver_solution.find_number_of_paths()
+
+        driver_reference.N = N
+        driver_reference.M = M
+        driver_reference.mine_field = mu.mine_field
+        reference_ans = driver_reference.find_number_of_paths()
+
+        return user_ans == reference_ans
+
+# solve the example case
+def task1():
+    return evaluate_input_solution("input/input0.txt")
+
+# N <= 20
+def task2():
+    task_result = True
+    task_result &= evaluate_solution(5,5)
+    task_result &= evaluate_solution(10,5)
+    task_result &= evaluate_solution(10,10)
+    task_result &= evaluate_solution(15,15)
+    task_result &= evaluate_solution(20,20)
+
+    return task_result
+
+# N <= 50
+def task3():
+    task_result = True
+    task_result &= evaluate_solution(20,30)
+    task_result &= evaluate_solution(30,30)
+    task_result &= evaluate_solution(40,30)
+    task_result &= evaluate_solution(40,40)
+    task_result &= evaluate_solution(50,40)
+    task_result &= evaluate_solution(50,50)
+
+    return task_result
+
+# N <= 100
+def task4():
+    task_result = True
+    task_result &= evaluate_solution(60,60)
+    task_result &= evaluate_solution(70,70)
+    task_result &= evaluate_solution(75,75)
+    task_result &= evaluate_solution(80,50)
+    task_result &= evaluate_solution(80,80)
+    task_result &= evaluate_solution(90,90)
+    task_result &= evaluate_solution(100,100)
+    task_result &= evaluate_solution(100,1)
+
+    return task_result
+
+print(task1())
+print(task2())
+print(task3())
+print(task4())
+
--- minato/solutions/cpp/solution.cpp
+++ minato/solutions/cpp/solution.cpp
@@ -1,19 +1,19 @@
 #define MAX_NUM 101
 
-int percorsi[MAX_NUM+1][MAX_NUM+1];
+int percorsi[MAX_NUM][MAX_NUM];
 
 int possible_paths(int row, int column)
 {
     if (percorsi[row][column] != -1) return percorsi[row][column];
 
-    if (row == N && column == M) return 1;
+    if (row == (N-1) && column == (M-1)) return 1;
 
     int tot = 0;
 
-    if ( row < N && mine_field[row +1][column] )
+    if ( row+1 < N && mine_field[row +1][column] == 0 )
         tot += possible_paths(row +1, column);
 
-    if ( column < M && mine_field[row][column+1] )
+    if ( column+1 < M && mine_field[row][column+1] == 0 )
         tot += possible_paths(row, column +1);
 
     percorsi[row][column] = tot;
@@ -22,9 +22,9 @@ int possible_paths(int row, int column)
 
 int find_number_of_paths()
 {
-    for (int i = 0; i <= N; ++i)
-        for (int j = 0; j <= M; ++j)
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
             percorsi[i][j] = -1;
 
-    return possible_paths(1,1);
+    return possible_paths(0,0);
 }
