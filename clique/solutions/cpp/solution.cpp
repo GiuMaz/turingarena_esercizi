8b864b85b8d0cefbad17a994f12191be7d7bc250
Giulio Mazzi <mazzi.giulio2@gmail.com>
2017-11-26 18:15:54 +0100

    partial works on clique

--- clique/solutions/cpp/solution.cpp
+++ clique/solutions/cpp/solution.cpp
@@ -1,4 +1,3 @@
-
 int isClique(int n, int* C, int** AdjG, int k)
 {
     int clique_size = 0;
@@ -33,9 +32,29 @@ int isIndependentSet(int n, int* S, int** AdjH, int h)
     return is_size == h ? 1 : 0;
 }
 
-void map_IS_into_Clique(int nH, const bool AdjH[MAX_N][MAX_N], int h,
-    int &nG, bool AdjG[MAX_N][MAX_N], int &k,
-const bool S[MAX_N] = 0, bool C[MAX_N] = 0)
+void map_IS_into_Clique(int nH, int** AdjH, int h, int nG, int** AdjG, int k, int* S, bool* C)
 {
 
+    nG = nH;
+
+    for (int i = 0; i < nH; ++i)
+        for (int j = 0; j < nH; ++j)
+            AdjG[i][j] = AdjH[i][j];
+
+    k = h;
+
+    for (int i = 0; i < nH; ++i)
+        S[i] = C[i];
 }
+
+void get_IS_cert_from_Clique_cert(int nH, int** AdjH, int h, bool S[MAX_N], const bool C[MAX_N])
+{
+    int nG; bool AdjG[MAX_N][MAX_N]; int k;
+    map_IS_into_Clique(nH, AdjH, h, nG, AdjG, k);
+    assert( isClique(nG, C, AdjG, k) );
+    // <-- ottieni un independent set S di H con |S|=h dalla clique C di G con |C|=k -->
+    // <-- in altre parole: fornire dimostrazione costruttiva del lemma difficile -->
+    map_IS_into_Clique(nH, AdjH, h, nG, AdjG, k, S, C);
+    assert( isIndependentSet(nH, S, AdjH, h) );
+}
+
