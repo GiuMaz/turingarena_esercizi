148424e99d656abb06c12d60830a4a201df5ef53
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-02-08 15:44:47 +0100

    improved solution in new version of light_coin

--- light_coin/entry.cpp
+++ light_coin/entry.cpp
@@ -4,6 +4,60 @@ void place(int coin, int position);
 
 int weigh();
 
-int find_light_coin() {
-    return 2;
+int find_light_coin()
+{
+    int start, middle, end, leftover;
+
+    leftover = N % 3;
+
+    start = 0;
+    middle = (N-leftover)/3;
+    end = middle * 2;
+
+    while (1)
+    {
+        if (N == 1) return start;
+
+        if (N == 2)
+        {
+            place(start, -1);
+            place(start+1, 1);
+
+            int ans =  weigh();
+
+            if (ans == -1) return start+1;
+            else return start;
+        }
+
+        for (int i = start; i < middle; ++i) place(i, -1);
+
+        for (int i = middle; i < end; ++i) place(i, 1);
+
+        int ans =  weigh();
+
+        if (ans == 0)
+        {
+            N = N - (end-start);
+            leftover = N%3;
+
+            start = end;
+        }
+        else if (ans == -1)
+        {
+            N = end-middle;
+            leftover = N%3;
+
+            start = middle;
+        }
+        else
+        {
+            N = middle-start;
+            leftover = N%3;
+
+            start = start;
+        }
+
+        middle = ((N-leftover)/3) + start;
+        end = 2*((N-leftover)/3) + start;
+    }
 }
